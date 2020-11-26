5798aa286665cc4f37a005732d5ca54d10a8377e
宇野礼於 <leouno12@gmail.com>
2020-11-26 14:59:29 +0900

    first commit

---
+++ components/Calendar.vue
@@ -0,0 +1,28 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+      },
+    }
+  },
+}
+</script>
+<style>
+@import '../node_modules/@fullcalendar/common/main.css';
+@import '../node_modules/@fullcalendar/daygrid/main.css';
+@import '../node_modules/@fullcalendar/timegrid/main.css';
+</style>
---
+++ pages/calendar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="columns is-mobile">
+      <full-calendar />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
